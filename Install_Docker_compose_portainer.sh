19a216bb4015c7e1993b438ab1162852f7343bd9
ajnasaboobacker <47502431+ajnasaboobacker@users.noreply.github.com>
2022-07-11 05:21:50 +0530

    Update Install_Docker_compose_portainer.sh

--- Install_Docker_compose_portainer.sh
+++ Install_Docker_compose_portainer.sh
@@ -6,7 +6,6 @@ echo -e "\033[31m
   )  (  )__)  )   / )(_) )\__ \ )(_) )/(__)\  _)(_  )   / \  /
   (_)\_)(____)(_)\_)(____/ (___/(____/(__)(__)(____)(_)\_) (__)
   \033[0m"
-
 echo ""
 echo ""
 
@@ -20,8 +19,10 @@ function check_internet() {
   printf "Checking if you have an active internet..."
   wget -q --spider http://github.com
   if [ $? -eq 0 ]; then
+    echo ""
     echo "Yes, You have! Lets do it!."
   else
+    echo ""
     error "Oops! You don't. Connect to a wired or wireless internet connection and try again."
   fi
 }
@@ -35,13 +36,26 @@ echo ""
 
 echo "Installing Docker-Compose.."
 sleep 5
+echo -ne '#####                     (19%)\r'
+sleep 1
+echo -ne '#############             (54%)\r'
+sleep 1
+echo -ne '#######################   (100%)\r'
+echo -ne '\n'
 echo ""
 
 sudo curl -L https://github.com/docker/compose/releases/download/1.29.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose || error "Failed to download Docker Compose."
 sudo chmod +x /usr/local/bin/docker-compose || error "Failed to set permission for Docker Compose"
 sleep 3
-
+echo ""
 echo "Installing Portainer.."
+echo -ne '#####                     (27%)\r'
+sleep 1
+echo -ne '#############             (66%)\r'
+sleep 1
+echo -ne '#######################   (100%)\r'
+echo -ne '\n'
+
 sleep 5
 echo ""
 
@@ -49,5 +63,5 @@ sudo docker pull portainer/portainer-ce:latest || error "Failed to pull latest P
 sudo docker run -d -p 9000:9000 -p 9443:9443 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest || error "Failed to run Portainer docker image!"
 echo""
 echo""
-echo"Installation Successful"
+echo "Installation Successful"
 echo "Please logout/Reboot for the changes to take effect."
